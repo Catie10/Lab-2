8e6bbd39a3eed5f3e164c24aeff4e8afea3faa48
Catie10 <haticetugbabayraktar@gmail.com>
2026-03-11 11:56:48 +0300

    guncelleme

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ void display(Node* head) {
     Node* temp = head;
 
     while (temp != NULL) {
-        cout << "[" << temp->data << "] -> ";
+        cout << temp->data << "->";
         temp = temp->next;
     }
 
